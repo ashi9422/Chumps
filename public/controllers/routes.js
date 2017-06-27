88086f94ec9696786958d0c7817f9476e9c20efe
Ashani Madhushika <Ashanikulasinghe@gmail.com>
2017-06-27 10:57:05 +0530

    supplier Add form

--- public/controllers/routes.js
+++ public/controllers/routes.js
@@ -4,16 +4,16 @@
 angular.module("myApp").config(function($routeProvider) {
     $routeProvider
         .when("/addsupplier", {
-            templateUrl : "views/supplier/form.html"
+            templateUrl : "views/supplier/supplierAdd.html"
         })
         .when("/suppliersview/:id", {
-            templateUrl : "views/supplier/updatesupplier.html"
+            templateUrl : "views/supplier/supplierUpdate.html"
         })
         .when("/viewsuppliers", {
-            templateUrl : "views/supplier/form.html"
+            templateUrl : "views/supplier/supplierView.html"
         })
         .when("/supplieradd", {
-            templateUrl : "views/supplier/supplieradd.html"
+            templateUrl : "views/supplier/supplierAdd.html"
         })
         .otherwise({
             template : "http://localhost:3000"
