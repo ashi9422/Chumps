3c0980fc06fd44308fce1800f4efc42293c7c74f
Ashani Madhushika <Ashanikulasinghe@gmail.com>
2017-06-30 16:53:28 +0530

    hari eka

--- server.js
+++ server.js
@@ -75,15 +75,10 @@ app.get('/login', (req, res, next) => {
 // });
 
 
-/*app.listen(3000, function (err) {
+app.listen(3000, function (err) {
     if(err){
         console.log(err);
     }else {
         console.log("server listen on port 3000")
     }
-});*/
-var port = process.env.PORT || 8000
- 
-app.listen(port, function() {
-    console.log("App is running on port " + port);
 });
